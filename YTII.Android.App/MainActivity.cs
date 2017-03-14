aa225ce8085550f74d6ea81f79f96cf191233e34
Joseph Smith <joseph.w.smith@gmail.com>
2017-03-14 08:51:50 -0500

    Initial Version / Commit

--- YTII.Android.App/MainActivity.cs
+++ YTII.Android.App/MainActivity.cs
@@ -1,19 +1,106 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using YTII.Android.App;
+using YTIIACL;
+using XamarinForms;
+using Android.Net;
+using System.Collections.Generic;
+using Android.Graphics;
+using System.Net;
+using Android.Content;
+using Android.Util;
+using Java.Lang;
 
 namespace YTII.Android.App
 {
     [Activity(Label = "YTII.Android.App", MainLauncher = true, Icon = "@drawable/icon")]
+    [IntentFilter(new[] { Intent.ActionView },
+        DataScheme = "http", DataHost = "youtube.com",
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Intent.ActionView },
+        DataScheme = "https", DataHost = "youtube.com",
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Intent.ActionView },
+        DataScheme = "http", DataHost = "youtu.be",
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Intent.ActionView },
+        DataScheme = "https", DataHost = "youtu.be",
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Intent.ActionView },
+        DataScheme = "vnd.youtube",
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+
+        protected XamarinForms.Models.YoutubeItem vid;
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            var closeButton = FindViewById<Button>(Resource.Id.closeButton);
+            closeButton.Click += CloseButton_Click;
+
+            //var da = Intent.GetStringExtra(Intent.ExtraText);
+            //Toast.MakeText(ApplicationContext, da, ToastLength.Long);
+
+            var vm = new XamarinForms.ViewModels.YoutubeViewModel();
+            var res = await vm.GetVideosDetailsAsync(new List<string>() { "TqwnP42cKZg" });
+
+            vid = res[0];
+
+            var textBlock = FindViewById<TextView>(Resource.Id.textView1);
+            textBlock.Text = vid.Title;
+
+            var imgHost = FindViewById<ImageView>(Resource.Id.imageView1);
+            Koush.UrlImageViewHelper.SetUrlDrawable(imgHost, vid.StandardThumbnailUrl);
+
+
+            var openButton = FindViewById<Button>(Resource.Id.button1);
+            openButton.Click += OpenButton_Click;
+            openButton.Enabled = true;
         }
+
+        private void CloseButton_Click(object sender, System.EventArgs e)
+        {
+            FinishAndRemoveTask();
+        }
+
+        private void OpenButton_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Intent i = new Intent(Intent.ActionView, Uri.Parse("vnd.youtube:" + vid.VideoId));
+                i.AddFlags(ActivityFlags.NewTask);
+                this.ApplicationContext.StartActivity(i);
+                FinishAfterTransition();
+                FinishAndRemoveTask();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("YTII", ex.Message);
+            }
+        }
+
+
+
+        private Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
+
     }
 }
 
